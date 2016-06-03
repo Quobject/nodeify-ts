1f4f357e31ed81707a29f2eda0322b8f6e6404c6
MatthiasL <matthias_ludwig@yahoo.com>
2016-06-04 04:55:44 +1000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+﻿export default function (promise: Promise<any>, callback?: (err, data) => void): Promise<any> {
+  if (typeof callback !== 'function') {
+    return promise;
+  }
+  promise.then(
+    function (result) {
+      callback(null, result);
+    }, function (error) {
+      callback(error || new Error, null);
+    }
+  );
+}
